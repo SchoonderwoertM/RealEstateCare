709e3f224e42ec8d727a8a35ce86d1f199043aea
Mariska <mariska.sdw@gmail.com>
2023-01-26 16:17:37 +0100

    first publish

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+    <AppHeader />
+    <HelloWorld msg="Hello World" />
+    <AppFooter />
+</template>
+
+<script>
+import AppHeader from "@/components/AppHeader.vue";
+import HelloWorld from "@/components/HelloWorld.vue";
+import AppFooter from "@/components/AppFooter.vue";
+
+export default {
+  name: "App",
+  components: {
+    AppHeader,
+    HelloWorld,
+    AppFooter,
+  },
+};
+</script>
+
+<style>
+@import '@/assets/css/main.css';
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #293439;
+}
+</style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,15 @@
+<template>
+  <header>
+    <div>
+      <div class="header-ellipse" />
+      <div class="bottom-ellipse" />
+    </div>
+    <img alt="Vue logo" src="@/assets/afbeeldingen/logo.png" />
+  </header>
+</template>
+
+<script>
+export default {
+  name: "AppHeader",
+};
+</script>
